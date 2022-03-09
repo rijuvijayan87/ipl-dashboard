6bd2ca52b8c521c07ba3347d1284f5f3ef6eaa24
Riju Vijayan <rijuvijayan@gmail.com>
2022-03-09 19:44:11 +1300

    initial commit with spring batch

---
+++ src/main/java/nz/co/id/ipldashboard/model/Match.java
@@ -0,0 +1,30 @@
+package nz.co.id.ipldashboard.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+public class Match {
+
+    @Id
+    private long id;
+    private String city;
+    private LocalDate date;
+    private String playerOfMatch;
+    private String venue;
+    private String team1;
+    private String team2;
+    private String tossWinner;
+    private String tossDecision;
+    private String matchWinner;
+    private String result;
+    private String resultMargin;
+    private String umpire1;
+    private String umpire2;
+}
